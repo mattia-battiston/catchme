45c084780f0a18b584b82902b2aa82b1dd8d6ecb
Mattia Battiston <mattia.battiston@gmail.com>
2012-02-01 21:29:50 +0100

    Show a map

--- android/src/com/catchme/CatchmeActivity.java
+++ android/src/com/catchme/CatchmeActivity.java
@@ -1,13 +1,21 @@
 package com.catchme;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class CatchmeActivity extends Activity {
+import com.google.android.maps.MapActivity;
+
+public class CatchmeActivity extends MapActivity {
+
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+  }
+
+  @Override
+  protected boolean isRouteDisplayed() {
+    // TODO Auto-generated method stub
+    return false;
   }
 }
