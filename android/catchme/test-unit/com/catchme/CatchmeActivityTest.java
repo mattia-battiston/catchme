bd40fcbbaed01464a1b3cb484d4fe1c801fce638
Mattia Battiston <mattia.battiston@gmail.com>
2012-02-04 12:02:06 +0100

    move project to a subfolder to better organize it

---
+++ android/catchme/test-unit/com/catchme/CatchmeActivityTest.java
@@ -0,0 +1,16 @@
+package com.catchme;
+
+import org.junit.Test;
+
+import android.test.AndroidTestCase;
+
+public class CatchmeActivityTest extends AndroidTestCase {
+
+  @Test
+  public void firstTest() {
+    assertTrue(true);
+
+    new CatchmeActivity();
+  }
+
+}
