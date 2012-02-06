0869a7f54f9b2bef4377318600d03d0fe64b03f5
Mattia Battiston <mattia.battiston@gmail.com>
2012-02-06 19:47:11 +0100

    try different solutions for tests

---
+++ android/catchme-unit-tests/src/com/catchme/CatchMeRobolectricTest.java
@@ -0,0 +1,26 @@
+package com.catchme;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(CustomTestRunner.class)
+public class CatchMeRobolectricTest {
+
+  @Test
+  public void routeIsNotDisplayed() {
+    CatchmeActivity activity = new CatchmeActivity();
+
+    assertThat(activity.isRouteDisplayed(), is(false));
+  }
+
+  @Test
+  public void onCreate() {
+    CatchmeActivity activity = new CatchmeActivity();
+
+    activity.onCreate(null);
+  }
+
+}
---
+++ android/catchme-unit-tests/src/com/catchme/CustomTestRunner.java
@@ -0,0 +1,16 @@
+package com.catchme;
+
+import java.io.File;
+
+import org.junit.runners.model.InitializationError;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+public class CustomTestRunner extends RobolectricTestRunner {
+
+  public CustomTestRunner(Class<?> testClass) throws InitializationError {
+    // defaults to "AndroidManifest.xml", "res" in the current directory
+    super(testClass, new File("../catchme"));
+  }
+
+}
