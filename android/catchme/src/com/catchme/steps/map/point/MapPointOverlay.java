cb8135e3315fc9a221414fb7c00a43eaf26c649d
Mattia Battiston <mattia.battiston@gmail.com>
2012-02-22 13:25:08 +0100

    draw point on the map

--- android/catchme/src/com/catchme/steps/map/point/MapPointOverlay.java
+++ android/catchme/src/com/catchme/steps/map/point/MapPointOverlay.java
@@ -7,11 +7,14 @@ import android.content.Context;
 import com.catchme.R;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
 
 public class MapPointOverlay extends ItemizedOverlay<OverlayItem> {
 
   private ArrayList<OverlayItem> overlayItems = new ArrayList<OverlayItem>();
+
+  private OverlayItem target;
 
   public MapPointOverlay(Context context) {
     super(boundCenterBottom(context.getResources().getDrawable(
@@ -19,7 +22,8 @@ public class MapPointOverlay extends ItemizedOverlay<OverlayItem> {
   }
 
   public void setPoint(GeoPoint point) {
-    overlayItems.add(new OverlayItem(point, "", ""));
+    this.target = new OverlayItem(point, "", "");
+    overlayItems.add(target);
     populate();
   }
 
@@ -31,6 +35,18 @@ public class MapPointOverlay extends ItemizedOverlay<OverlayItem> {
   @Override
   public int size() {
     return overlayItems.size();
+  }
+
+  @Override
+  public boolean onTap(GeoPoint p, MapView mapView) {
+    System.out.println("tap sul punto " + p);
+    return super.onTap(p, mapView);
+  }
+
+  @Override
+  protected boolean onTap(int index) {
+    System.out.println("tap sull'oggetto " + index);
+    return super.onTap(index);
   }
 
 }
