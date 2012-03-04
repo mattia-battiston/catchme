ed2f2afe926b19b48e40a093b8cd862e37fa399d
Mattia Battiston <mattia.battiston@gmail.com>
2012-03-04 17:19:10 +0100

    refactoring welcome step

--- android/catchme-unit-tests/src/com/catchme/steps/text/FirstPointReachedTest.java
+++ android/catchme-unit-tests/src/com/catchme/steps/text/FirstPointReachedTest.java
@@ -1,9 +1,18 @@
 package com.catchme.steps.text;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
 import com.catchme.base.BaseUnitTest;
 
 public class FirstPointReachedTest extends BaseUnitTest {
 
-  FirstPointReached firstPointReached = FirstPointReached.getInstance();
+  FirstPointReached presenter = FirstPointReached.getInstance();
+
+  @Test
+  public void hasName() {
+    assertNotNull(presenter.getName());
+  }
 
 }
--- android/catchme/src/com/catchme/steps/text/FirstPointReached.java
+++ android/catchme/src/com/catchme/steps/text/FirstPointReached.java
@@ -1,14 +1,8 @@
 package com.catchme.steps.text;
 
-import android.content.Context;
-import android.content.Intent;
-import android.view.View;
+import com.catchme.steps.text.type.BaseTextStepPresenter;
 
-import com.catchme.flow.presenter.BasePresenter;
-import com.catchme.steps.text.type.TextStepActivity;
-import com.catchme.steps.text.type.TextStepView;
-
-public class FirstPointReached extends BasePresenter<TextStepView> {
+public class FirstPointReached extends BaseTextStepPresenter {
 
   private static FirstPointReached instance = new FirstPointReached();
 
@@ -17,27 +11,6 @@ public class FirstPointReached extends BasePresenter<TextStepView> {
 
   public static FirstPointReached getInstance() {
     return instance;
-  }
-
-  @Override
-  public void go(Context context) {
-    // TODO this method could be extracted in a BaseTestStepPresenter
-    Intent goToWelcome = intentFactory.createIntent(context,
-        TextStepActivity.class);
-    context.startActivity(goToWelcome);
-  }
-
-  @Override
-  public void bind(final TextStepView view) {
-    // TODO this method could be extracted in a BaseTestStepPresenter
-    view.getContinueButton().setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        next(view.getContext());
-      }
-    });
-
-    // TODO set text
   }
 
   @Override
