38e8cc57513045e9a064fbbef4f356ebbddf7da6
Mattia Battiston <mattia.battiston@gmail.com>
2012-03-17 17:50:16 +0100

    configuring first steps

--- android/catchme/src/com/catchme/flow/step/StepRetriever.java
+++ android/catchme/src/com/catchme/flow/step/StepRetriever.java
@@ -32,6 +32,7 @@ public class StepRetriever {
     steps.add(new SimplePointPresenter("ferraris", 45433293, 10996181));
     steps.add(new SimplePointPresenter("firstPizza", 45439135, 10992768));
     steps.add(new SimplePointPresenter("oldHouse", 45439290, 10985363));
+    steps.add(new SimplePointPresenter("cinemaFiume", 45442627, 10976893));
     steps.add(new SimplePointPresenter("pozzoAmore", 45442993, 10996096));
     steps.add(new SimplePointPresenter("viaStella", 45441716, 10998894));
     steps.add(new SimplePointPresenter("pontePietra", 45447705, 10999932));
